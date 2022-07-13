94d66a07c0fc8382d377ea95a9dfaa64c21d91a7
ODabila <106769233+ODabila@users.noreply.github.com>
2022-07-13 19:07:36 +0000

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-/**
-*void prototypes()
-*int prototypes()
-*char prototypes()
-*/
+
 int _putchar(char c);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
