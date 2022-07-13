04ebd03a9288a483ab88f53b7e0ffc5a4d846097
ODabila <106769233+ODabila@users.noreply.github.com>
2022-07-13 05:38:25 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,39 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *
- *  * puts2 - function that prints every other character of a string, starting
- *
- *   *with the first character, followed by a new line.
- *
- *    * @str:  Pointer of a char type.
- *
- *     * Return: void.
- *
- *      */
-
+ * puts2 - function that prints every other character of a string, starting
+ *with the first character, followed by a new line.
+ * @str:  Pointer of a char type.
+ * Return: void.
+ */
 void puts2(char *str)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			if (str[0] != '\0')
-
-					{
-
-								do {
-
-												_putchar(str[i]);
-
-															i = i + 2;
-
-																	} while ((str[i - 1] != '\0') && (str[i] != '\0'));
-
-									}
-
-				_putchar('\n');
-
+	if (str[0] != '\0')
+	{
+		do {
+			_putchar(str[i]);
+			i = i + 2;
+		} while ((str[i - 1] != '\0') && (str[i] != '\0'));
+	}
+	_putchar('\n');
 }
