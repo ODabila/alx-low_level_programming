e69765c1f0396fde119dc0e3a69b0b450eb0e995
ODabila <106769233+ODabila@users.noreply.github.com>
2022-07-13 05:15:39 +0000

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,76 +1,39 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
+ *rev_string -  function that prints a string, in reverse,
+ *followed by a new line.
+ *@s : pointer to a string.
  *
- * *rev_string -  function that prints a string, in reverse,
- *
- * *followed by a new line.
- *
- * *
- *
- * *@s : pointer to a string.
- *
- * * Return: string lenght.
- *
- * */
-
+ *Return: string lenght.
+ */
 void rev_string(char *s)
-
 {
-
-		int len = _strlen(s), i;
-
-			char *rev = s;
-
-				char tmp;
-
-
-
-					while (*(rev++) != '\0')
-
-								continue;
-
-						rev = rev - 2;
-
-							for (i = 0; i < len / 2; ++i)
-
-									{
-
-												tmp = s[i];
-
-														s[i] = rev[0];
-
-																rev[0] = tmp;
-
-																		rev--;
-
-																			}
-
+	int len = _strlen(s), i;
+	char *rev = s;
+	char tmp;
+	
+	while (*(rev++) != '\0')
+		continue;
+	rev = rev - 2;
+	for (i = 0; i < len / 2; ++i)
+	{
+		tmp = s[i];
+		s[i] = rev[0];
+		rev[0] = tmp;
+	rev--;
+	}
 }
-
 /**
- *
- * * _strlen - function that that returns the length of a string.
- *
- * * @s : pointer to a string.
- *
- * * Return: string lenght
- *
- * */
-
+ *_strlen - function that that returns the length of a string.
+ *@s : pointer to a string.
+ *Return: string lenght
+ */
 int _strlen(char *s)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (s[i] != '\0')
-
-						i++;
-
-				return (i);
-
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
