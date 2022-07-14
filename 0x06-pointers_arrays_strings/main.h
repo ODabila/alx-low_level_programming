62f669ebffcaa4a3a239af7e230a04a3c7d6d41d
ODabila <dabilaouattara80@gmail.com>
2022-07-14 23:18:18 +0000

    code

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *);
-char *rot13(char *str);
+char *rot13(char *);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+void print_buffer(char *b, int size);
+void print_number(int n);
 #endif
