6a6193b48cfc06781417bcc35e69c54bd85c0f56
ODabila <dabilaouattara80@gmail.com>
2022-07-15 03:34:15 +0000

    change

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ char *rot13(char *);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 void print_buffer(char *b, int size);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
