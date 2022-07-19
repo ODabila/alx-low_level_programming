454c4299b4337cb6af4f6926ba48e8933cb37f96
ODabila <dabilaouattara80@gmail.com>
2022-07-19 05:59:32 +0000

    secod code

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strstr -  This function that locates a substring.
+*
+*@haystack: Pointer to the array.
+*@needle: A ponter to a match string.
+*
+*Return: Returns a pointer to the beginning of the located substring, or NULL
+*if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int  j = 0;
+	unsigned int lenNeedle = _strlen(needle);
+	unsigned int count = 0;
+
+	while (haystack[0] != '\0')
+	{
+		for (j = 0; j < lenNeedle; ++j)
+		{
+			if (haystack[j] == needle[j])
+			{
+				count++;
+			}
+		}
+		if (count == lenNeedle)
+			return (haystack);
+		count = 0;
+		haystack++;
+	}
+	return (0);
+}
+/**
